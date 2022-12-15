88592bec01772ffe397f143368342ad6f7222bca
Tomasz Jeznach <tjeznach@rivosinc.com>
2022-12-15 15:05:53 -0800

    RIVOS: update merge script
    Signed-off-by: Tomasz Jeznach <tjeznach@rivosinc.com>

--- rivos_create_next.sh
+++ rivos_create_next.sh
@@ -3,19 +3,42 @@
 set -e
 
 ID=`date +%Y%m%d.%H%M`
+THIS_BRANCH=`git rev-parse --abbrev-ref HEAD`
 NEXT_BRANCH="dev/tjeznach/next/${ID}"
 
-TAG_BASE="v6.1-rc7"
+# Override base tag with -b <tag>.
+TAG_BASE="v6.1"
+# Override tag name with -t <tag>.
 TAG_NEXT="tjeznach/rivos-next"
+# Push to remote repo if -p flag added.
+GIT_PUSH=0
 
 COMPONENTS=( \
-    'origin/dev/bend/rivos-v6.1-rc3/feature/rivos-main' \
-    'avpatel-linux/riscv_kvm_aia_v1' \
+    # RIVOS CI scripts.
+    'origin/dev/tjeznach/rivos/ci' \
+    # mirror of https://github.com/avpatel/linux/tree/riscv_kvm_aia_v1
+    'origin/dev/tjeznach/feature/riscv-kvm-aia' \
+    # mirror of https://github.com/tjeznach/linux/tree/tjeznach/riscv-iommu
     'origin/dev/tjeznach/feature/riscv-iommu' \
+    # QEMU device driver: edu.
     'origin/dev/tjeznach/feature/qemu-edu' \
-    'origin/dev/tjeznach/feature/dpa' \
+    # RIVOS device drivers: DPA, DCE.
+    'origin/dev/sonny/feature/dpa' \
     'origin/dev/bend/feature/dce' \
 )
+
+while getopts "b:pk" arg; do
+  case $arg in
+  b ) TAG_BASE=$OPTARG ;;
+  t ) TAG_NEXT=$OPTARG ;;
+  p ) GIT_PUSH=1 ;;
+  * ) echo "$0 [-b base_tag] [-p]"
+      echo "  -b tag  : use tag as a merge parent"
+      echo "  -p      : push merged branch to remote repo"
+      exit 0
+      ;;
+  esac
+done
 
 # Checkout new merge branch based on current 'next' tag.
 git checkout -b "${NEXT_BRANCH}" "${TAG_BASE}"
@@ -23,6 +46,8 @@ git checkout -b "${NEXT_BRANCH}" "${TAG_BASE}"
 # Report an error and drop local integration branch.
 die() {
     echo "Integraion branch '${NEXT_BRANCH}' creation failed."
+    git log
+    git checkout --force "${THIS_BRANCH}"
     git branch -D "${NEXT_BRANCH}"
     exit 1
 }
@@ -31,12 +56,14 @@ trap die ERR
 
 # Merge feature tags
 for C in ${COMPONENTS[@]}; do
-    git merge -m "RIVOS: merge ${C} into ${NEXT_BRANCH}" --log=100 "${C}"
+    git merge -m "RIVOS: merge ${C} into ${NEXT_BRANCH}" --log=100 --no-ff "${C}"
 done
 
-git tag -fa -m "Automerge for ${NEXT_BRANCH}" "${TAG_NEXT}" 
-git push origin :refs/tags/${TAG_NEXT}
-git push origin HEAD --tags
+if [[ $GIT_PUSH -eq 1 ]]; then
+  git tag -fa -m "Automerge for ${NEXT_BRANCH}" "${TAG_NEXT}"
+  git push origin :refs/tags/${TAG_NEXT}
+  git push origin HEAD --tags
+fi
 
 TAG_HASH=`git rev-parse HEAD`
 
