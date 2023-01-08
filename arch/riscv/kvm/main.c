c64453878a3f536a5f8d08e66932bf162213eb72
Atish Patra <atishp@rivosinc.com>
2023-01-08 03:29:14 -0800

    RISC-V: KVM: Perform limited operations in hardware enable/disable
    Hardware enable/disable path only need to perform AIA/NACL enable/disable
    for TVMs. All other operations i.e. interrupt/exception delegation,
    counter access must be provided by the TSM as host doesn't have control
    of these operations for a TVM.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>

--- arch/riscv/kvm/main.c
+++ arch/riscv/kvm/main.c
@@ -13,6 +13,7 @@
 #include <asm/hwcap.h>
 #include <asm/kvm_nacl.h>
 #include <asm/sbi.h>
+#include <asm/kvm_cove.h>
 
 long kvm_arch_dev_ioctl(struct file *filp,
 			unsigned int ioctl, unsigned long arg)
@@ -39,6 +40,10 @@ int kvm_arch_hardware_enable(void)
 	if (rc)
 		return rc;
 
+	/* We just need to invoke aia enable for CoVE */
+	if (unlikely(kvm_riscv_cove_enabled()))
+		goto enable_aia;
+
 	hedeleg = 0;
 	hedeleg |= (1UL << EXC_INST_MISALIGNED);
 	hedeleg |= (1UL << EXC_BREAKPOINT);
@@ -58,6 +63,7 @@ int kvm_arch_hardware_enable(void)
 
 	csr_write(CSR_HVIP, 0);
 
+enable_aia:
 	kvm_riscv_aia_enable();
 
 	return 0;
@@ -67,6 +73,8 @@ void kvm_arch_hardware_disable(void)
 {
 	kvm_riscv_aia_disable();
 
+	if (unlikely(kvm_riscv_cove_enabled()))
+		goto disable_nacl;
 	/*
 	 * After clearing the hideleg CSR, the host kernel will receive
 	 * spurious interrupts if hvip CSR has pending interrupts and the
@@ -78,6 +86,7 @@ void kvm_arch_hardware_disable(void)
 	csr_write(CSR_HEDELEG, 0);
 	csr_write(CSR_HIDELEG, 0);
 
+disable_nacl:
 	kvm_riscv_nacl_disable();
 }
 
